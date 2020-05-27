51abfcdaa55c47a0752f81fe9c2015403a2f8040
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-27 20:35:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,16 +1,13 @@
 class EmailAddressParser
+  attr_accessor :email
+    def  initialize(emails)
+      @email = emails
+    end
 
-  attr_accessor :emails
+    def parse
+      email_array = @email.split(/[, ]/).uniq
+      email_array.reject! {|element| element.empty?}
+      email_array
+    end
 
-  def initialize(emails)
-    @emails = emails
   end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
-
-end
